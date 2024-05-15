04cb4c5fe401a9fa96b2cb058b143dfb5ea52c95
Jasminferolino <162878994+ferolinojasmin@users.noreply.github.com>
2024-05-15 18:57:32 +0800

    add ferolino_jasmin file

---
+++ ferolino_jasmin/javascript.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("button");
+var textarea = document.getElementById("textarea");
+var text = document.getElementById("text");
+
+function checkInputs() {
+  if (textarea.value.trim() !== "" && text.value.trim() !== "") {
+    button.disabled = false; // Enable the button
+  } else {
+    button.disabled = true; // Disable the button
+  }
+}
+
+textarea.addEventListener("input", checkInputs);
+text.addEventListener("input", checkInputs);
